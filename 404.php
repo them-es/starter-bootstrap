8372612eb4d19fd3f035bea3acb58821a831f5af
them.es <themes@klicc.at>
2015-04-27 11:05:55 +0200

    classname changed

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
     get_header();
 ?>
 
-	<div id="post-0" class="post error404 not-found">
+	<div id="post-0" class="content error404 not-found">
 		<h1 class="entry-title"><?php _e( 'Not Found', 'my-theme' ); ?></h1>
 		<div class="entry-content">
 			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'my-theme' ); ?></p>
