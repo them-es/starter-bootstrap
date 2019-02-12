a575411a30e233ef3b12ac0481a8bf4b9650a1e1
them-es <them-es@users.noreply.github.com>
2019-02-12 17:34:23 +0100

    Update single.php

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
 	<?php
 		if ( have_posts() ) :
 			while ( have_posts() ) :
-			the_post();
+				the_post();
 		?>
 		
 		<?php
@@ -36,6 +36,8 @@
 			<li><?php previous_post_link( '%link', '<span aria-hidden="true">&larr;</span> ' . __( 'Previous Post', 'my-theme' ) ); ?></li>
 			<li><?php next_post_link( '%link', __( 'Next Post', 'my-theme' ) . ' <span aria-hidden="true">&rarr;</span>' ); ?></li>
 		</ul><!-- /.pager -->
-	<?php endif; ?>
+	<?php
+		endif;
+	?>
 
 <?php get_footer(); ?>
