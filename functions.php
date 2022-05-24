2838d9e6abc08a1f234904f1e191006d51c9b8e4
them-es <themes@klicc.at>
2022-05-24 08:30:28 +0200

    Code modernization
    Bump "Tested up to WordPress 6.0"

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  *
  * @since v1.0
  */
-$theme_customizer = get_template_directory() . '/inc/customizer.php';
+$theme_customizer = __DIR__ . '/inc/customizer.php';
 if ( is_readable( $theme_customizer ) ) {
 	require_once $theme_customizer;
 }
@@ -16,7 +16,7 @@ if ( is_readable( $theme_customizer ) ) {
  * 
  * @since v1.0
  */
-$theme_wordpresscom = get_template_directory() . '/inc/wordpresscom.php';
+$theme_wordpresscom = __DIR__ . '/inc/wordpresscom.php';
 if ( is_readable( $theme_wordpresscom ) ) {
 	require_once $theme_wordpresscom;
 }
@@ -40,7 +40,7 @@ if ( ! isset( $content_width ) ) {
 if ( ! function_exists( 'themes_starter_setup_theme' ) ) :
 	function themes_starter_setup_theme() {
 		// Make theme available for translation: Translations can be filed in the /languages/ directory.
-		load_theme_textdomain( 'my-theme', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'my-theme', __DIR__ . '/languages' );
 
 		// Theme Support.
 		add_theme_support( 'title-tag' );
@@ -466,12 +466,12 @@ if ( function_exists( 'register_nav_menus' ) ) {
 }
 
 // Custom Nav Walker: wp_bootstrap_navwalker().
-$custom_walker = get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
+$custom_walker = __DIR__ . '/inc/wp_bootstrap_navwalker.php';
 if ( is_readable( $custom_walker ) ) {
 	require_once $custom_walker;
 }
 
-$custom_walker_footer = get_template_directory() . '/inc/wp_bootstrap_navwalker_footer.php';
+$custom_walker_footer = __DIR__ . '/inc/wp_bootstrap_navwalker_footer.php';
 if ( is_readable( $custom_walker_footer ) ) {
 	require_once $custom_walker_footer;
 }
@@ -486,15 +486,15 @@ function themes_starter_scripts_loader() {
 	$theme_version = wp_get_theme()->get( 'Version' );
 
 	// 1. Styles.
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), $theme_version, 'all' );
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css', array(), $theme_version, 'all' ); // main.scss: Compiled Framework source + custom styles.
+	wp_enqueue_style( 'style', get_theme_file_uri( 'style.css' ), array(), $theme_version, 'all' );
+	wp_enqueue_style( 'main', get_theme_file_uri( 'assets/css/main.css' ), array(), $theme_version, 'all' ); // main.scss: Compiled Framework source + custom styles.
 
 	if ( is_rtl() ) {
-		wp_enqueue_style( 'rtl', get_template_directory_uri() . '/assets/css/rtl.css', array(), $theme_version, 'all' );
+		wp_enqueue_style( 'rtl', get_theme_file_uri( 'assets/css/rtl.css' ), array(), $theme_version, 'all' );
 	}
 
 	// 2. Scripts.
-	wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/assets/js/main.bundle.js', array(), $theme_version, true );
+	wp_enqueue_script( 'mainjs', get_theme_file_uri( 'assets/js/main.bundle.js' ), array(), $theme_version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
