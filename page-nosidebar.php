2521d75bfb416de3bb52b5539f004e5b72e4f3ed
them-es <themes@klicc.at>
2022-07-07 10:18:40 +0200

    Update templates
    Make "Full width" template really full width
    Create a "No Sidebar" template

---
+++ page-nosidebar.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Page (No Sidebar)
+ * Description: Page template with no sidebar.
+ *
+ */
+
+get_header();
+
+the_post();
+?>
+<div id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
+	<h1 class="entry-title"><?php the_title(); ?></h1>
+	<?php
+		the_content();
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . __( 'Pages:', 'my-theme' ),
+			'after'  => '</div>',
+		) );
+		edit_post_link( __( 'Edit', 'my-theme' ), '<span class="edit-link">', '</span>' );
+	?>
+</div><!-- /#post-<?php the_ID(); ?> -->
+<?php
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+		comments_template();
+	endif;
+
+get_footer();
