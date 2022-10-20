d8a3db2ae3055a7c628500f9fd77e8da94bf25b4
Richard Willis-Owen <richard@willis-owen.co.uk>
2022-10-20 17:41:18 +0100

    fix order-sm-last in classes for id=sidebar

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 
 if ( is_active_sidebar( 'primary_widget_area' ) || is_archive() || is_single() ) :
 ?>
-<div id="sidebar" class="col-md-4 order-md-first col-sm-12 oder-sm-last">
+<div id="sidebar" class="col-md-4 order-md-first col-sm-12 order-sm-last">
 	<?php
 		if ( is_active_sidebar( 'primary_widget_area' ) ) :
 	?>
