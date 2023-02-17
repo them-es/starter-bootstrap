a326af152d3272ee5ffbca9b67fde2f0c779fb8a
them-es <them-es@users.noreply.github.com>
2023-02-17 09:17:16 +0100

    Tested up to WordPress 6.2
    Mark as compatible with WordPress 6.2
    Replace date_i18n() with wp_date()

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6">
-						<p><?php printf( esc_html__( '&copy; %1$s %2$s. All rights reserved.', 'my-theme' ), date_i18n( 'Y' ), get_bloginfo( 'name', 'display' ) ); ?></p>
+						<p><?php printf( esc_html__( '&copy; %1$s %2$s. All rights reserved.', 'my-theme' ), wp_date( 'Y' ), get_bloginfo( 'name', 'display' ) ); ?></p>
 					</div>
 
 					<?php
